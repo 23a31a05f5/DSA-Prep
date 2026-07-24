aee1a9e1863e7ed914516a2790e4fc812eea72d4
23a31a05f5 <kalyanilakshmipolisetty05@gmail.com>
2026-07-24 15:59:52 +0530

    updated

--- 02_patterns/pattern.py
+++ 02_patterns/pattern.py
@@ -443,5 +443,38 @@ pattern20(5)
 # **      **
 # *        *
         
+def pattern21(n):
+        for i in range(n):
+            for j in range(n):
+                if j==0 or j==n-1 or i==0 or i==n-1:
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+            print()    
+pattern21(5)  
+# o/p:
+# *****
+# *   *
+# *   *
+# *   *
+# *****
 
-         
+def pattern22(n):
+        for i in range(2*n-1):
+            for j in range(2*n-1):
+                top=i
+                left=j
+                rifht=(2*n-2)-j
+                bottom=(2*n-2)-i
+                print(n-min(top,bottom,left,rifht),end=" ")
+            print()
+pattern22(5)
+# o/p:
+# 5 4 4 4 4 4 4 4 5 
+# 5 4 3 3 3 3 3 4 5 
+# 5 4 3 2 2 2 3 4 5 
+# 5 4 3 2 1 2 3 4 5 
+# 5 4 3 2 2 2 3 4 5 
+# 5 4 3 3 3 3 3 4 5 
+# 5 4 4 4 4 4 4 4 5 
+# 5 5 5 5 5 5 5 5 5
