24d450fe80fceacb0bf5ddfc62f4855f12f29d16
23a31a05f5 <kalyanilakshmipolisetty05@gmail.com>
2026-08-21 21:45:59 +0530

    medium

--- 06_Arrays/Medium/Maximum_subarray_sum(Kadanes).py
+++ 06_Arrays/Medium/Maximum_subarray_sum(Kadanes).py
@@ -41,25 +41,7 @@ def maximumsubsum(arr):
     return max_s
 print(maximumsubsum([-2,-3,4,-1,-2,1,5,-3]))
 #o/p:7
-# if they want the subarray also
-def maximumsubsum(arr):
-    s=0
-    max_s=0
-    ansstart=-1
-    ansend=-1
-    for i in range(len(arr)):
-        if s==0:
-            start=i
-        s+=arr[i]
-        if s<0:
-            s=0
-        elif s>max_s:
-            max_s=max(max_s,s)
-            ansstart=start
-            ansend=i
-    return arr[ansstart:ansend+1],max_s
-print(maximumsubsum([-2,-3,4,-1,-2,1,5,-3]))
-#o/p:([4, -1, -2, 1, 5],7)
+
 
 
 #kandens for negative also
