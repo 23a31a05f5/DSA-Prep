6e504fcec7e7a3bd27a5b23a1c87f7afde040bed
23a31a05f5 <kalyanilakshmipolisetty05@gmail.com>
2026-08-23 12:31:11 +0530

    medium

---
+++ 06_Arrays/Medium/leader_in_arr.py
@@ -0,0 +1,34 @@
+#time:o(n^2)
+#space:o(n)
+def findleader(arr):
+    ans=[]
+    for i in range(len(arr)):
+        leader=True
+        for j in range(i+1,len(arr)):
+            if arr[i]<arr[j]:
+                leader=False
+                break
+        if leader==True:
+            ans.append(arr[i])
+    return ans
+
+print(findleader([10,22,6,12,0,6]))
+#[22,12,6]
+
+#optimal
+#time:o(n)
+#space:o(n)
+def findleader(arr):
+    n=len(arr)
+    maxi=arr[-1]
+    ans=[maxi]
+    for i in range(n-1,0,-1):
+        if arr[i]>maxi:
+            ans.append(arr[i])
+            maxi=arr[i]
+    ans.reverse()
+    return ans
+
+
+print(findleader([10,22,6,12,0,6]))
+#[22,12,6]
