79f10c519821decb5f6b4ab54e0de322325a640b
23a31a05f5 <kalyanilakshmipolisetty05@gmail.com>
2026-08-26 18:31:08 +0530

    medium

--- 06_Arrays/Medium/leader.py
+++ 06_Arrays/Medium/leader.py
@@ -21,14 +21,12 @@ print(findleader([10,22,6,12,0,6]))
 def findleader(arr):
     n=len(arr)
     maxi=arr[-1]
-    ans=[maxi]
+    res=[maxi]
     for i in range(n-1,0,-1):
         if arr[i]>maxi:
-            ans.append(arr[i])
+            res.append(arr[i])
             maxi=arr[i]
-    ans.reverse()
-    return ans
-
-
+    res.reverse()
+    return res
 print(findleader([10,22,6,12,0,6]))
 #[22,12,6]
